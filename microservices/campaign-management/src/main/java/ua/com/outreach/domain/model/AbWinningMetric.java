41189aa2810b173ef74ce7f17cd9ffcdb4b010c6
Ivan Paniotov <ivanpaniotov@gmail.com>
2024-06-30 14:08:27 +0300

    remove service

--- microservices/campaign-management/src/main/java/ua/com/outreach/domain/model/AbWinningMetric.java
+++
@@ -1,9 +0,0 @@
-package ua.com.outreach.domain.model;
-
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-
-public enum AbWinningMetric {
-    CLICKS,
-    CONVERSIONS,
-}
